d2a21af634c3175ecc81087f5952bdca7dc5fdde
Andrew Clemens <apc@press.jhu.edu>
2018-05-07 11:29:24 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,220 @@
+<template>
+  <div id="app">
+    <Showtimes msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Showtimes from './components/Showtimes.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Showtimes
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Anton|IBM+Plex+Mono|Muli');
+
+body, html {
+    font-family: 'Muli', sans-serif;
+}
+
+input, textarea, select {
+    font-family: inherit;
+}
+
+.theatre-checkbox {
+    display: none;
+}
+
+.theatre-label {
+    cursor: pointer;
+    color: red;
+}
+.theatre-checkbox:checked + label {
+    color: green;
+}
+
+h2, h4 {
+    font-family: 'Acme', sans-serif;
+}
+h2 {
+    border-top: 2px solid;
+    padding: 5px 0;
+}
+
+.hidden-movie {
+    cursor: pointer;
+}
+#removed-movies ul {
+    list-style-type: none;
+}
+
+#sidebar {
+    position: fixed;
+    width: 320px;
+    height: 100%;
+    z-index: 1;
+}
+
+.tooltip {
+    border-bottom: 1px dotted black;
+}
+
+.tooltip .tooltiptext {
+    visibility: hidden;
+    background-color: black;
+    color: #fff;
+    padding: 5px 0;
+    border-radius: 6px;
+    position: absolute;
+    z-index: 1;
+}
+.tooltiptext {
+    max-width: 720px;
+    display: inline;
+}
+.tooltiptext ul {
+    list-style-type: none;
+    padding: 5px 15px;
+}
+.tooltiptext img.poster {
+    float: left;
+}
+.tooltiptext div {
+    padding: 5px 0;
+}
+.tooltiptext.summary {
+    width: 720px;
+    padding: 10px;
+}
+.tooltiptext.summary img {
+    padding-right: 10px;
+}
+
+.tooltip:hover .tooltiptext {
+    visibility: visible;
+}
+
+#select-all-label {
+    display:inline-block;
+    padding-bottom: 10px;
+    color: blue;
+}
+
+#movie-list {
+    margin-left: 325px;
+    width: calc(100% - 320px);
+}
+#movie-list thead tr:nth-child(1) th{
+    position: -webkit-sticky;
+    position: -moz-sticky;
+    position: -ms-sticky;
+    position: -o-sticky;
+    position: sticky;
+    top: 10px;
+}
+#movie-list ul {
+    list-style-type: none;
+}
+#movie-list td, #movie-list th {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+#movie-list tbody tr:not(.visible) {
+    display: none;
+}
+#movie-list tbody .visible:nth-of-type(even) {
+    background-color: #f2f2f2;
+}
+#movie-list tbody .visible:nth-of-type(odd) {
+    background-color: #ddd;
+}
+.visible:hover {
+    background-color: white !important;
+}
+#movie-list th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: black;
+    color: white;
+}
+
+.external-link {
+    width: 20px;
+}
+.external-link-wrapper {
+    float: right;
+    margin: 15px;
+    background-color: white;
+    margin-left: 5px;
+}
+
+.sortable {
+    cursor: pointer;
+}
+.sorted-down:after {
+    content: '▲';
+    float: right;
+}
+.sorted-up:after {
+    content: '▼';
+    float: right;
+}
+
+#location-form {
+    font-family: 'IBM Plex Mono', monospace;
+    display: grid;
+    grid-template-rows: 20px 30px 20px 30px 20px 30px 20px 30px 40px;
+    grid-template-columns: 260px 60px;
+}
+#zip-label {
+    grid-column: 1 / 3;
+    grid-row: 1 / 2;
+}
+
+#zip {
+    grid-column: 1 / 3;
+    grid-row: 2 / 3;
+}
+#distance-label {
+    grid-column: 1 / 3;
+    grid-row: 3 / 4;
+}
+#distance {
+    width: 240px;
+    grid-column: 1 / 2;
+    grid-row: 4 / 5;
+}
+#unit-select {
+    grid-column: 2 / 3;
+    grid-row: 4 / 5;
+}
+#datefrom-label {
+    grid-column: 1 / 3;
+    grid-row: 5 / 6;
+}
+#datefrom {
+    grid-column: 1 / 3;
+    grid-row: 6 / 7;
+}
+#dateto-label {
+    grid-column: 1 / 3;
+    grid-row: 7 / 8;
+}
+#dateto {
+    grid-column: 1 / 3;
+    grid-row: 8 / 9;
+}
+#submit {
+    grid-column: 1 / 3;
+    grid-row: 9 / 10;
+    margin-top: 10px;
+}
+
+
+</style>
---
+++ src/components/Showtimes.vue
@@ -0,0 +1,371 @@
+<template>
+<div>
+    <div id="sidebar">
+        <form id="location-form" v-on:submit.prevent="onLocationSubmit">
+            <label id="zip-label" for="zip">zip code:</label>
+            <input type="text" pattern="[0-9]{5}" name="zip" id="zip" required>
+            <label id="distance-label" for="distance">radius:</label>
+            <input type="number" name="distance" id="distance" value="15" required>
+            <select name="unit" id="unit-select">
+                <option value="mi">mi</option>
+                <option value="km">km</option>
+            </select>
+            <label id="datefrom-label" for="datefrom">start date:</label>
+            <input type="date" name="datefrom" id="datefrom" required>
+            <label id="dateto-label" for="dateto">end date:</label>
+            <input type="date" name="dateto" id="dateto" required>
+            <input id="submit" type="submit" value="submit">
+        </form>
+        <div id="sk-circle" class="sk-circle" v-show="loading">
+            <div class="sk-circle1 sk-child"></div>
+            <div class="sk-circle2 sk-child"></div>
+            <div class="sk-circle3 sk-child"></div>
+            <div class="sk-circle4 sk-child"></div>
+            <div class="sk-circle5 sk-child"></div>
+            <div class="sk-circle6 sk-child"></div>
+            <div class="sk-circle7 sk-child"></div>
+            <div class="sk-circle8 sk-child"></div>
+            <div class="sk-circle9 sk-child"></div>
+            <div class="sk-circle10 sk-child"></div>
+            <div class="sk-circle11 sk-child"></div>
+            <div class="sk-circle12 sk-child"></div>
+        </div>
+        <div id="theatre-list">
+            <h2>Theatres</h2>
+            <div v-show="theatres.length" class="theatre-label" id="select-all-label" v-on:click="toggleAll">Select all</div>
+            <div v-for="(data) in theatres">
+                <input class="theatre-checkbox" type="checkbox" :id="data.id" :value="data.id" v-model="checkedTheatres">
+                <label class="theatre-label" :for="data.id">{{data.name}}</label>
+            </div>
+        </div>
+        <div id="removed-movies">
+            <h2>Hidden movies</h2>
+            <ul>
+                <li class="hidden-movie" v-for="(data) in removedMovies" v-on:click="reAddMovie(data.rootId)">
+                    {{ data.title }}
+                </li>
+            </ul>
+        </div>
+    </div>
+	<table id="movie-list">
+        <thead>
+		<tr class="visible">
+			<th>Title</th>
+			<th>Theatres</th>
+			<th>Genres</th>
+		</tr>
+        </thead>
+        <tbody>
+            <tr v-for="(data) in movies" :key="data.tmsId" v-bind:class="{visible: showMovie(data)}">
+			<td>
+                <div class="remove-button" v-on:click="removedMovies.push(data)"></div>
+                <span class="tooltip">
+                      {{data.title}}
+                      <div class="tooltiptext summary">
+                <div class="external-link-wrapper">
+                    <a target="_blank" :href="'http://www.google.com/search?q=site:en.wikipedia.org+' + data.title.replace(' ', '+') + '+'+data.releaseYear + '+film&btnI'">
+                        <img class="external-link" src="./wiki.png">
+                    </a>
+                    <a target="_blank" :href="'http://www.google.com/search?q=site:rottentomatoes.com+' + data.title.replace(' ', '+') + '+'+data.releaseYear + '+film&btnI'">
+                        <img class="external-link" src="./rt.png">
+                    </a>
+                    <a target="_blank" :href="'http://www.google.com/search?q=site:imdb.com+' + data.title.replace(' ', '+') + '+'+data.releaseYear + '+film&btnI'">
+                        <img class="external-link" src="./imdb.png">
+                    </a>
+                    <a target="_blank" :href="'http://www.google.com/search?q=site:youtube.com+trailer+' + data.title.replace(' ', '+') + '+'+data.releaseYear + '+film&btnI'">
+                        <img class="external-link" src="./youtube.jpg">
+                    </a>
+                </div>
+                          <h3>{{ data.title }}</h3>
+                          <img class="poster" :src="'https://dlby.tmsimg.com/' + data.preferredImage.uri">
+                          <div v-if="data.directors"><strong>Directed by:</strong> {{ data.directors.join(', ') }}</div>
+                          <div v-if="data.topCast"><strong>Cast:</strong> {{data.topCast.join(', ')}}</div>
+                          <div v-if="data.releaseDate"><strong>Release date:</strong> {{ data.releaseDate }}</div>
+                          <div v-if="data.genres"><strong>Genres:</strong> {{data.genres.join(', ')}}</div>
+                          <div v-if="data.runTime"><strong>Runtime:</strong> {{ formatRuntime(data.runTime) }}</div>
+                          <div></div>
+                          <div v-if="data.longDescription">{{ data.longDescription }}</div>
+                      </div>
+                </span>
+            </td>
+			<td>
+                <div class="tooltip" v-for="(showtimes, theatre) in formatShowtimes(data.showtimes)">
+                    {{theatre}}
+                    <span class="tooltiptext">
+                        <ul>
+                            <li v-for="(showtime) in showtimes">{{formatShowtime(showtime)}}</li>
+                        </ul>
+                    </span>
+                </div>
+            </td>
+            <td><span v-if="data.genres">{{data.genres.join(', ')}}</span></td>
+		</tr>
+        </tbody>
+	</table>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Showtimes',
+  data() {
+      return {
+          movies: [],
+          theatres: [],
+          checkedTheatres: [],
+          removedMovies: [],
+
+          loading: false,
+      }
+  },
+  methods: {
+      toggleAll: function(evt) {
+          console.log(this.checkedTheatres);
+          console.log(this.theatres);
+          switch (evt.target.innerHTML) {
+              case 'Select all':
+                  this.checkedTheatres = [];
+                  for (var i=0; i<this.theatres.length; i++) {
+                      this.checkedTheatres.push(this.theatres[i].id);
+                  }
+                  evt.target.innerHTML = 'Select none';
+                  break;
+              case 'Select none':
+                  this.checkedTheatres = [];
+                  evt.target.innerHTML = 'Select all';
+                  break;
+          }
+      },
+      formatRuntime: function(runtime) {
+          var hr = parseInt(runtime.substr(2,2));
+          var min = parseInt(runtime.substr(5,2));
+          return '' + (60*hr+min) + ' minutes';
+      },
+      formatShowtime: function(showtime) {
+          var a = new Date(showtime);
+          return a.toDateString() + ' ' + a.toLocaleTimeString();
+      },
+      showMovie: function(movie) {
+          if (findWithAttr(this.removedMovies, 'title', movie.title) != -1) return false;
+          var ct = this.checkedTheatres;
+          var ret = false;
+          movie.showtimes.forEach(function(showtime){
+              // if showtime.theatre.id in checkedTheatres, return true
+              if (ct.indexOf(showtime.theatre.id) >= 0) {
+                  ret = true;
+                  return;
+              }
+          })
+          return ret;
+      },
+      reAddMovie: function(rootId) {
+          var index = findWithAttr(this.removedMovies, 'rootId', rootId);
+          this.removedMovies.splice(index, 1);
+      },
+      formatShowtimes: function(showtimes) {
+          var displayedShowtimes = {};
+          var ct = this.checkedTheatres
+          showtimes.forEach(function(showtime){
+              if (ct.indexOf(showtime.theatre.id) == -1) return;
+              if (!displayedShowtimes.hasOwnProperty(showtime.theatre.name)) displayedShowtimes[showtime.theatre.name] = [];
+              displayedShowtimes[showtime.theatre.name].push(showtime.dateTime);
+          })
+          return displayedShowtimes;
+      },
+      onLocationSubmit: function(evt) {
+          this.loading = true;
+          var ths = this;
+          var theatres = this.theatres;
+          var zip = evt.target[0].value;
+          var distance = evt.target[1].value;
+          var unit = evt.target[2].value;
+          var startDate = evt.target[3].value;
+          var endDate = evt.target[4].value;
+
+          var a = new Date(startDate);
+          var b = new Date(endDate);
+          var numDays = Math.round(Math.abs((b.getTime() - a.getTime())/(24*60*60*1000)));
+
+          var data = {
+              startDate: startDate,
+              numDays: numDays,
+              zip: zip,
+              radius: distance,
+              units: unit,
+              api_key: apikey,
+          };
+          var url = theatersUrl + '?';
+          for (var prop in data) {
+              url += prop + '=' + data[prop] + '&';
+          }
+          var xhr = new XMLHttpRequest();
+          xhr.open('GET', url);
+          xhr.onload = function() {
+              if (xhr.status === 200) {
+                  ths.movies = JSON.parse(xhr.responseText);
+                  ths.movies.sort(function(a, b){
+                      return a.title > b.title;
+                  })
+                  console.log(ths.movies);
+
+                  ths.movies.forEach(function(movie){
+                      movie.showtimes.forEach(function(showtime){
+                          if (findWithAttr(theatres, 'id', showtime.theatre.id) == -1) theatres.push(showtime.theatre);
+                      })
+                  })
+              } else console.log(xhr);
+              ths.loading = false;
+          }
+          xhr.send();
+      },
+  }
+}
+
+
+// helper functions
+function findWithAttr(array, attr, value) {
+    for (var i=0; i<array.length; i++) {
+        if (array[i][attr] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+var apikey = "h6p3tbv6f2u5savqzhgq5wts";
+var baseUrl = "https://data.tmsapi.com/v1.1";
+var theatersUrl = baseUrl + '/movies/showings';
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.remove-button {
+    background-image: url('./remove.png');
+    height: 16px;
+    width: 16px;
+    background-size: cover;
+    display: inline-block;
+    cursor: pointer;
+}
+.sk-circle {
+  margin: 100px auto;
+  width: 40px;
+  height: 40px;
+  position: relative;
+}
+.sk-circle .sk-child {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+.sk-circle .sk-child:before {
+  content: '';
+  display: block;
+  margin: 0 auto;
+  width: 15%;
+  height: 15%;
+  background-color: #333;
+  border-radius: 100%;
+  -webkit-animation: sk-circleBounceDelay 1.2s infinite ease-in-out both;
+          animation: sk-circleBounceDelay 1.2s infinite ease-in-out both;
+}
+.sk-circle .sk-circle2 {
+  -webkit-transform: rotate(30deg);
+      -ms-transform: rotate(30deg);
+          transform: rotate(30deg); }
+.sk-circle .sk-circle3 {
+  -webkit-transform: rotate(60deg);
+      -ms-transform: rotate(60deg);
+          transform: rotate(60deg); }
+.sk-circle .sk-circle4 {
+  -webkit-transform: rotate(90deg);
+      -ms-transform: rotate(90deg);
+          transform: rotate(90deg); }
+.sk-circle .sk-circle5 {
+  -webkit-transform: rotate(120deg);
+      -ms-transform: rotate(120deg);
+          transform: rotate(120deg); }
+.sk-circle .sk-circle6 {
+  -webkit-transform: rotate(150deg);
+      -ms-transform: rotate(150deg);
+          transform: rotate(150deg); }
+.sk-circle .sk-circle7 {
+  -webkit-transform: rotate(180deg);
+      -ms-transform: rotate(180deg);
+          transform: rotate(180deg); }
+.sk-circle .sk-circle8 {
+  -webkit-transform: rotate(210deg);
+      -ms-transform: rotate(210deg);
+          transform: rotate(210deg); }
+.sk-circle .sk-circle9 {
+  -webkit-transform: rotate(240deg);
+      -ms-transform: rotate(240deg);
+          transform: rotate(240deg); }
+.sk-circle .sk-circle10 {
+  -webkit-transform: rotate(270deg);
+      -ms-transform: rotate(270deg);
+          transform: rotate(270deg); }
+.sk-circle .sk-circle11 {
+  -webkit-transform: rotate(300deg);
+      -ms-transform: rotate(300deg);
+          transform: rotate(300deg); }
+.sk-circle .sk-circle12 {
+  -webkit-transform: rotate(330deg);
+      -ms-transform: rotate(330deg);
+          transform: rotate(330deg); }
+.sk-circle .sk-circle2:before {
+  -webkit-animation-delay: -1.1s;
+          animation-delay: -1.1s; }
+.sk-circle .sk-circle3:before {
+  -webkit-animation-delay: -1s;
+          animation-delay: -1s; }
+.sk-circle .sk-circle4:before {
+  -webkit-animation-delay: -0.9s;
+          animation-delay: -0.9s; }
+.sk-circle .sk-circle5:before {
+  -webkit-animation-delay: -0.8s;
+          animation-delay: -0.8s; }
+.sk-circle .sk-circle6:before {
+  -webkit-animation-delay: -0.7s;
+          animation-delay: -0.7s; }
+.sk-circle .sk-circle7:before {
+  -webkit-animation-delay: -0.6s;
+          animation-delay: -0.6s; }
+.sk-circle .sk-circle8:before {
+  -webkit-animation-delay: -0.5s;
+          animation-delay: -0.5s; }
+.sk-circle .sk-circle9:before {
+  -webkit-animation-delay: -0.4s;
+          animation-delay: -0.4s; }
+.sk-circle .sk-circle10:before {
+  -webkit-animation-delay: -0.3s;
+          animation-delay: -0.3s; }
+.sk-circle .sk-circle11:before {
+  -webkit-animation-delay: -0.2s;
+          animation-delay: -0.2s; }
+.sk-circle .sk-circle12:before {
+  -webkit-animation-delay: -0.1s;
+          animation-delay: -0.1s; }
+
+@-webkit-keyframes sk-circleBounceDelay {
+  0%, 80%, 100% {
+    -webkit-transform: scale(0);
+            transform: scale(0);
+  } 40% {
+    -webkit-transform: scale(1);
+            transform: scale(1);
+  }
+}
+
+@keyframes sk-circleBounceDelay {
+  0%, 80%, 100% {
+    -webkit-transform: scale(0);
+            transform: scale(0);
+  } 40% {
+    -webkit-transform: scale(1);
+            transform: scale(1);
+  }
+}
+</style>
